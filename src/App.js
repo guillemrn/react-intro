45e56a750072a67ede6421f53661bc0238da610f
Guillermo Moreno <guillermolpzm@gmail.com>
2023-07-19 19:22:21 -0600

    components creaded

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearchField } from './TodoSearchField';
 import { TodoList } from './TodoList';
@@ -5,21 +6,34 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
 
+const defaultTodos = [
+  { text: "Hacer tarea", completed: false },
+  { text: "Hacer de comer", completed: true },
+  { text: "Curso react", completed: false },
+  { text: "Crear dieta", completed: true },
+  { text: "Jugar con max", completed: false },
+  { text: "Jugar con maggie", completed: false }
+]
+
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={15} total={20} />
       <TodoSearchField />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
 
-    </div>
+    </React.Fragment>
   );
 }
 
